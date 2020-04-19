461fbea4715ad01eb7c006fa2c697c6af269aa30
Thenukaheshith98 <Thenukaheshith@gmail.com>
2020-04-19 16:29:50 +0530

    my 2nd commit

--- PAFHospitalManagamentSystem/src/main/java/com/PAFHospitalManagamentSystem/PAFHospitalManagamentSystem/AppointmentResource.java
+++ PAFHospitalManagamentSystem/src/main/java/com/PAFHospitalManagamentSystem/PAFHospitalManagamentSystem/AppointmentResource.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -26,11 +29,32 @@ public class AppointmentResource {
 		
 	}
 	
+	@GET
+	@Path("/Appointmentview")
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.TEXT_HTML})
+	public Appointment ViewAllAppointmnet(Appointment a1){
+		return ar.ViewAllAppointmnet(a1);
+	}
+	
 	@POST
 	@Path("/Appointment")
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.TEXT_HTML})
 	public Appointment CreateAppointment(Appointment a1) {
 		return ar.CreateAppointment(a1);
 	}
+	
+	@PUT
+	@Path("/AppointmentUpdate")
+	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.TEXT_HTML})
+	public Appointment updateAppointment(Appointment a1) {
+		return ar.updateAppointment(a1);
+	}
+	
+	@DELETE 
+	@Path("/Appointmentdelete")
+	@Consumes({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.TEXT_HTML})
+	public Appointment DeleteAppointment(Appointment a1) {
+		return ar.DeleteAppointment(a1);
+	} 
 	
 }
--- PAFHospitalManagamentSystem/src/main/java/com/PAFHospitalManagamentSystem/PAFHospitalManagamentSystem/AppointmnetRepository.java
+++ PAFHospitalManagamentSystem/src/main/java/com/PAFHospitalManagamentSystem/PAFHospitalManagamentSystem/AppointmnetRepository.java
@@ -27,49 +27,105 @@ public class AppointmnetRepository {
 		appointment = new ArrayList<Appointment>();
 		
 		Appointment a1 = new Appointment();
+		a1.setP_id(1);
 		a1.setP_name("g.h. perera");
 		a1.setP_age(34);
 		a1.setType_of_case("fewer");
 		a1.setD_name("nakawita i.m.");
 		
-		Appointment a2 = new Appointment();
-		a2.setP_name("t.i alwis");
-		a2.setP_age(14);
-		a2.setType_of_case("knee pain");
-		a2.setD_name("perera k.f");
-		
-		Appointment a3 = new Appointment();
-		a3.setP_name("w.d. jennifer");
-		a3.setP_age(56);
-		a3.setType_of_case("migraine");
-		a3.setD_name("premasiri r.n.");
+//		Appointment a2 = new Appointment();
+//		a2.setP_id(2);
+//		a2.setP_name("t.i alwis");
+//		a2.setP_age(14);
+//		a2.setType_of_case("knee pain");
+//		a2.setD_name("perera k.f");
+//		
+//		Appointment a3 = new Appointment();
+//		a3.setP_id(3);
+//		a3.setP_name("w.d. jennifer");
+//		a3.setP_age(56);
+//		a3.setType_of_case("migraine");
+//		a3.setD_name("premasiri r.n.");
 		
 		appointment.add(a1);
-		appointment.add(a2);
-		appointment.add(a3);
-		
+//		appointment.add(a2);
+//		appointment.add(a3);
+//		
 	}
 	
 	public List<Appointment> getAllAppointments() {
 		return appointment;
 	}
 	
+	
+	public Appointment ViewAllAppointmnet(Appointment a1) {
+		String viewsql = "SELECT * FROM `appointmnets` WHERE `p_id`=?";
+		try {
+			PreparedStatement st = con.prepareStatement(viewsql);
+			st.setInt(1, a1.p_id);
+			st.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return a1;
+	}
+	
+	
 	public Appointment CreateAppointment(Appointment a1) {
-		String insertSql = "INSERT INTO `appointmnets`(`p_name`, `p_age`, `type_of_case`, `d_name`) VALUES (?,?,?,?)";
+		String insertSql = "INSERT INTO `appointmnets`(`p_id`, `p_name`, `p_age`, `type_of_case`, `d_name`) VALUES (?,?,?,?,?)";
 		try {
 			PreparedStatement st = con.prepareStatement(insertSql);
-			st.setString(1, a1.p_name);
-			st.setInt(2, a1.p_age);
-			st.setString(3, a1.type_of_case);
-			st.setString(4, a1.d_name);
+			st.setInt(1, a1.p_id);
+			st.setString(2, a1.p_name);
+			st.setInt(3, a1.p_age);
+			st.setString(4, a1.type_of_case);
+			st.setString(5, a1.d_name);
 			
 			st.executeUpdate();
 		}catch(Exception e) {
 			System.out.println(e);
 		}
 		
-		appointment.add(a1);
-		System.out.println(appointment); 
+		
+		return a1;
+	}
+	
+	public Appointment updateAppointment(Appointment a1) {
+		String updateSql = "UPDATE `appointmnets` SET `p_name`=?,`p_age`=?,`type_of_case`=?,`d_name`=? WHERE `p_id`=?";
+		try {
+			PreparedStatement st = con.prepareStatement(updateSql);
+			
+			st.setString(1, a1.p_name);
+			st.setInt(2, a1.p_age);
+			st.setString(3, a1.type_of_case);
+			st.setString(4, a1.d_name);
+			st.setInt(5, a1.p_id);
+			
+			st.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+
+		
+		return a1;
+	}
+	
+	public Appointment DeleteAppointment(Appointment a1) {
+		String deleteSql = "DELETE FROM `appointmnets` WHERE `p_id`=?";
+		try {
+			PreparedStatement st = con.prepareStatement(deleteSql);
+			
+		
+			st.setInt(1, a1.p_id);
+			
+			st.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+
+		
 		return a1;
 	}
 }
+
